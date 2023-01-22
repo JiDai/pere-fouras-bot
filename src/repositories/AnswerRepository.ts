afcde8b240215b90e700e9c5f5c4cbc0845dbbab
Jordi Dosne <jidai@dosne.net>
2023-01-22 16:34:47 +0100

    feat: add board

--- src/repositories/AnswerRepository.ts
+++ src/repositories/AnswerRepository.ts
@@ -2,7 +2,15 @@ import type {definitions} from '../types/supabase.ts';
 
 import {SupabaseClient} from "https://deno.land/x/supabase/mod.ts";
 
+import riddles from "../data/riddles.json" assert {type: "json"};
+
 type BaseAnswerEntity = definitions['pf_answer'];
+type RiddleEntity = {
+	"id": string,
+	"question": Array<string>,
+	"show": string,
+	"answer": string
+}
 
 export interface AnswerEntity extends BaseAnswerEntity {
 	question_id: string;
@@ -18,6 +26,11 @@ export default class AnswerRepository {
 		this.dbClient = dbClient;
 	}
 
+	getCurrent(): Promise<RiddleEntity> {
+		const index = Math.floor(Math.random() * riddles.length);
+		return Promise.resolve(riddles[index]);
+	}
+
 	async post(answer: string, username: string, valid: boolean, questionId: string): Promise<boolean> {
 		const {data, error} = await this.dbClient.from('pf_answer')
 			.insert({
@@ -27,5 +40,37 @@ export default class AnswerRepository {
 				valid
 			});
 		return !(error || data.length === 0);
+	}
+
+	async getBoard(): Promise<Record<string, number>> {
+		const {data, error} = await this.dbClient.from('pf_answer')
+			.select('*')
+			.eq('valid', true)
+			.limit(5);
+
+		if (error || !data) {
+			console.error('Cannot get board');
+			return {};
+		}
+
+		const result: Record<string, number> = {};
+		for (let i = 0; i < data.length; i++) {
+			const item = data[i];
+			const key = item.username;
+			if (!result[key]) {
+				result[key] = 0;
+			}
+
+			if (item.valid) {
+				result[key] += 1;
+			}
+		}
+
+		const sortedResult: Record<string, number> = {};
+		Object.keys(result)
+			.sort((a, b) => result[b] - result[a]) // sort by descending order
+			.forEach(key => sortedResult[key] = result[key]);
+
+		return sortedResult;
 	}
 }
